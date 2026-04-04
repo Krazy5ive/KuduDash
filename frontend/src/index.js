6873d33c2f7e6d3f6c8d2b34a4122ae4d8257eb2
Makoena Mabotja <makoenasally62@gmail.com>
2026-04-04 19:33:20 +0200

    feat: wrap app with BrowserRouter for React Router support

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <BrowserRouter>
-    <Auth0Provider
-      domain={process.env.REACT_APP_AUTH0_DOMAIN}
-      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-      authorizationParams={{
-        redirect_uri: `${window.location.origin}/vibe`,
-        audience: process.env.REACT_APP_AUTH0_AUDIENCE,
-      }}
-    >
-      <App />
-    </Auth0Provider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Auth0Provider
+        domain={process.env.REACT_APP_AUTH0_DOMAIN}
+        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+        authorizationParams={{
+          redirect_uri: `${window.location.origin}/vibe`,
+          audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+        }}
+      >
+        <App />
+      </Auth0Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
+
+reportWebVitals();
