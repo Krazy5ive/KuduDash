f8677c336cb227b14f366eead30966aefc74e041
Sazi <mphosazi@gmail.com>
2026-04-04 13:33:34 +0200

    div replaced

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -11,36 +11,36 @@ const Login = () => {
       <section className="login__card">
 
         <header className="login__header">
-          <div className="login__logo" aria-hidden="true"></div>
+          <span className="login__logo" aria-hidden="true"></span>
           <h1 className="login__title">KuduDash</h1>
         </header>
 
         <p className="login__tagline">Less waiting. More eating.</p>
 
         <p className="login__description">
-            Skip the line. Eat well. Study better.
+          Skip the line. Eat well. Study better.
         </p>
 
         <button
-            className="login__button"
-            onClick={handleGoogleLogin}
+          className="login__button"
+          onClick={handleGoogleLogin}
         >
-            <img
+          <img
             src="https://cdn-icons-png.flaticon.com/512/2991/2991148.png"
             alt=""
             aria-hidden="true"
             className="login__button-icon"
-            />
-            <span>Sign in with Google</span>
+          />
+          Sign in with Google
         </button>
 
         <footer className="login__footer">
-            <p>New here or back again? We got you.</p>
+          <p>New here or back again? We got you.</p>
         </footer>
 
-        </section>
+      </section>
     </main>
-    );
+  );
 };
 
 export default Login;
