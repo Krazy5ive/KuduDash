345dabfbd0ccc9cf2e57dc3ca0ba5984bf8aea4c
Panaashe <2725508@students.wits.ac.za>
2026-04-15 21:50:44 +0200

    Created CartIcon.js and css

---
+++ frontend/src/Cart/CartIcon.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useCart } from "../context/CartContext";
+import "./CartIcon.css";
+
+const CartIcon = () => {
+  const { cartCount } = useCart();
+
+  return (
+    <Link to="/cart" className="kd-cart-icon-link">
+      <span className="kd-cart-icon">🛒</span>
+      {cartCount > 0 && (
+        <output className="kd-cart-badge">{cartCount}</output>
+      )}
+    </Link>
+  );
+};
+
+export default CartIcon;
