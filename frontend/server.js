34b98c304e00c6f9a5eac69b206a53bee1ee0ffc
thabogabe <gabethabo23@gmail.com>
2026-05-01 14:52:51 +0200

    fix: change frontend server port from 8080 to 3000

--- frontend/server.js
+++ frontend/server.js
@@ -7,7 +7,7 @@ const require = createRequire(import.meta.url)
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 const app = express()
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3000
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
