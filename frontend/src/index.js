55c9e9bee27ac82e8dbce0e908f21918385beeac
Sazi <mphosazi@gmail.com>
2026-05-19 14:54:43 +0200

    reverse useRefreshTokens removal

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -18,6 +19,7 @@ root.render(
           redirect_uri: `${window.location.origin}/callback`,
           audience: process.env.REACT_APP_AUTH0_AUDIENCE,
         }}
+        useRefreshTokens={true}
         cacheLocation="localstorage"
       >
         <App />
